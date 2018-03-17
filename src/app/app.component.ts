4682eee42c66c215cef294dd413eb1c3f1949e8a
Sam Vendittelli <sam.vendittelli@hotmail.com>
2018-03-17 19:55:28 +0000

    Add title, pages, and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title = 'My Website';
+	beginTypingSubtitle: Boolean = false;
+
+	onComplete() {
+		setTimeout(() => this.beginTypingSubtitle = true, 500);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TypingAnimationDirective } from 'angular-typing-animation';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		TypingAnimationDirective,
+		AboutComponent,
+		HomeComponent,
+		ContactComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		AppRoutingModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
