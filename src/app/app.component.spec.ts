d532975a015986de36ce6de57f55b51ca6cf4b8a
Sam Vendittelli <sam.vendittelli@hotmail.com>
2018-07-21 11:35:57 +0100

    Add angular material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { HeaderComponent, FooterComponent } from './layout';
+import { FooterComponent, HeaderComponent } from './layout';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [
-                AppComponent,
-                HeaderComponent,
-                FooterComponent
-            ],
-            imports: [
-                RouterTestingModule
-            ]
+            declarations: [AppComponent, HeaderComponent, FooterComponent],
+            imports: [RouterTestingModule, MatToolbarModule, MatButtonModule]
         }).compileComponents();
     }));
+
     it('should create the app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HeaderComponent, FooterComponent } from './layout';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
@@ -20,9 +24,12 @@ import { ContactComponent } from './contact/contact.component';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
         AppRoutingModule
     ],
     providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
