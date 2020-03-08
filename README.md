2e434a2a76741e00a9038df593f5b6a9a36bd9b3
Sam Vendittelli <sam.vendittelli@hotmail.com>
2020-03-08 15:09:10 +0000

    Move to vanilla js and gulp

--- README.md
+++ README.md
@@ -1,32 +1,9 @@
-# My Website
+# Personal Website
 
 [![Website](https://img.shields.io/website-up-down-green-red/http/svendittelli.github.io.svg)](https://svendittelli.github.io/)
 [![Travis](https://img.shields.io/travis/SVendittelli/personal-website-ui.svg)](https://travis-ci.org/SVendittelli/personal-website-ui)
-[![Codecov](https://img.shields.io/codecov/c/github/SVendittelli/personal-website-ui.svg)](https://codecov.io/gh/SVendittelli/personal-website-ui)
-[![GitHub issue last update](https://img.shields.io/github/last-commit/SVendittelli/personal-website-ui.svg)](https://github.com/SVendittelli/personal-website-ui)
+[![GitHub last updated](https://img.shields.io/github/last-commit/SVendittelli/personal-website-ui.svg)](https://github.com/SVendittelli/personal-website-ui)
 [![GitHub tag](https://img.shields.io/github/tag/SVendittelli/personal-website-ui.svg)](https://github.com/SVendittelli/personal-website-ui/releases)
 [![license](https://img.shields.io/github/license/SVendittelli/personal-website-ui.svg)](https://github.com/SVendittelli/personal-website-ui/blob/master/LICENSE)
 
 You can find my website [here](https://svendittelli.github.io/).
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.0.
-
-## Development server
-
-Run `npm run start` for a dev server. Navigate to [localhost:4200](http://localhost:4200/). The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `npm run build` to build the project. The build artefacts will be stored in the `dist/` directory. Use the `-prod` flag for a production build.
-
-## Running unit tests
-
-Run `npm test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `npm e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
---
+++ gulpfile.ts
@@ -0,0 +1,123 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import htmlmin from 'gulp-htmlmin';
+import inject from 'gulp-inject';
+import rename from 'gulp-rename';
+import sass from 'gulp-sass';
+import uglify from 'gulp-uglify';
+import postcss from 'gulp-postcss';
+import svgmin from 'gulp-svgmin';
+
+import browserSync from 'browser-sync';
+import cssnano from 'cssnano';
+import del from 'del';
+import prefix from 'autoprefixer';
+
+const server = browserSync.create();
+
+const paths = {
+  input: 'src/',
+  output: 'dist/',
+  scripts: {
+    input: 'src/js/*',
+    output: 'dist/js/'
+  },
+  styles: {
+    input: 'src/sass/**/*.{scss,sass}',
+    output: 'dist/css/'
+  },
+  svgs: {
+    input: 'src/assets/**/*.svg',
+    output: 'dist/assets/'
+  },
+  html: {
+    input: 'src/**/*.html',
+    output: 'dist/**/*.html'
+  },
+  copy: [
+    'src/**/*',
+    '!src/js/*',
+    '!src/sass/**/*',
+    '!src/assets/**/*.svg',
+    '!src/index.html'
+  ],
+  serve: './dist/'
+};
+
+const clean = () => del(paths.output);
+
+function html() {
+  return gulp.src(paths.html.input).pipe(gulp.dest(paths.output));
+}
+
+function styles() {
+  return gulp
+    .src(paths.styles.input)
+    .pipe(sass())
+    .pipe(postcss([prefix()]))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(postcss([cssnano()]))
+    .pipe(gulp.dest(paths.styles.output));
+}
+
+function js() {
+  return gulp
+    .src(paths.scripts.input)
+    .pipe(babel())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.output));
+}
+
+function svg() {
+  return gulp
+    .src(paths.svgs.input)
+    .pipe(svgmin())
+    .pipe(gulp.dest(paths.svgs.output));
+}
+
+function move() {
+  return gulp.src(paths.copy).pipe(gulp.dest(paths.output));
+}
+
+const copy = gulp.parallel(html, styles, js, svg, move);
+
+function injectDependencies() {
+  const SOURCES = gulp.src(
+    [paths.styles.output + '*', paths.scripts.output + '*'],
+    {
+      read: false
+    }
+  );
+  return gulp
+    .src(paths.html.output)
+    .pipe(inject(SOURCES, { relative: true }))
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true
+      })
+    )
+    .pipe(gulp.dest(paths.output));
+}
+
+function serve(done: (error?: any) => void) {
+  server.init({
+    server: {
+      baseDir: paths.serve
+    }
+  });
+  done();
+}
+
+function reload(done: (error?: any) => void) {
+  server.reload();
+  done();
+}
+
+const build = gulp.series(copy, injectDependencies);
+const watch = () => gulp.watch(paths.input, gulp.series(build, reload));
+
+export const prod = gulp.series(clean, build);
+export const dev = gulp.series(clean, build, serve, watch);
+export default dev;
